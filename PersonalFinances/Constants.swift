145090bb441a8da3b46a980901b7051615163ced
Alexander Shpin <@lexhpin@gmail.com>
2022-09-11 14:46:47 -0500

    Welcome Screen set up

---
+++ PersonalFinances/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  PersonalFinances
+//
+//  Created by Alexander Shpin on 11.09.2022.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    
+    
+    struct BrandColors {
+        static let lightPurple = "BrandLightPurple"
+    }
+}
--- PersonalFinances/Controller/WelcomeViewController.swift
+++ PersonalFinances/Controller/WelcomeViewController.swift
@@ -7,11 +7,22 @@
 
 import Foundation
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet var appLabel: CLTypingLabel!
+    
+    
     override func viewDidLoad() {
-        //
+        appLabel.text = "Personal Finances"
+        appLabel.charInterval = 0.08
+    }
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
     }
     
 }
